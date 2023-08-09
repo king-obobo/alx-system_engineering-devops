6b0fd3c5026e3bc7d6e83bd7c61c624d2e8bb285
king-obobo <oboboebuka1@gmail.com>
2023-08-08 19:31:25 -0700

    Attempted s fix for 3. Count it!

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -29,8 +29,8 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
     count += results.get("dist")
     for c in results.get("children"):
         title = c.get("data").get("title").lower().split()
-        for word in word_list:
-            if word.lower() in title:
+        for word in set([w.lower() for w in word_list]):
+            if word in title:
                 times = len([t for t in title if t == word.lower()])
                 if instances.get(word) is None:
                     instances[word] = times
